79285dc8eddcd3812e28e3620093d38ecffb0631
NgoThanhCong <ntc2882@gmail.com>
2019-04-18 22:19:19 +0700

    hello

---
+++ session1/index.js
@@ -0,0 +1,24 @@
+// let name = prompt("X")
+// console.log("hello","name");
+
+let x = prompt("nhap vao so thu 1");
+let y = prompt("nhap vao so thu 2");
+
+console.log(Number(x) + Number(y));
+
+// 1. tạo file html
+// 2. gõ html , chọn html:5 để hiển thị khung hình
+//3. tạo file js để code js
+//4. nối 2 file với nhau thông qua script
+//5.chạy file html
+//6.inspect ( kiểm tra ) trên trình duyệt (chrome)
+//7. chuyển qua tab console để xem kết quả
+
+
+// loops
+for(let i = 0; i < 5; i++){
+    console.log (i);
+    console.log("hello")
+}
+
+// ++ tăng thêm 1 đơn vị
